80e48db773c8f221551523db8fd10e430feebedc
Max Heisinger <mail@maxheisinger.at>
2025-08-14 16:40:33 +0200

    Only do a test if Glibc is used
    This test is handling multiplicites and there is some issue with musl
    libc.

--- test/test_siftup.cpp
+++ test/test_siftup.cpp
@@ -23,6 +23,7 @@ TEST_CASE("heap_siftup_uint32") {
   std::array<int32_t, 8> arr, arr2;
   std::fill(arr.begin(), arr.end(), std::numeric_limits<int32_t>::max());
 
+#ifdef __GLIBC__
   heap_siftup(arr.data(), arr.size(), 10);
   heap_siftup(arr.data(), arr.size(), 20);
   REQUIRE(std::is_heap(arr.begin(), arr.end()));
@@ -44,4 +45,5 @@ TEST_CASE("heap_siftup_uint32") {
   REQUIRE(std::is_heap(arr.begin(), arr.end()));
   heap_siftup(arr.data(), arr.size(), 8);
   REQUIRE(std::is_heap(arr.begin(), arr.end()));
+#endif
 }
